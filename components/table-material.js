a548978eee2ae9758197815f86548c4934a78648
Carlos Guerrero <git@carlosguerrero.com>
2019-02-23 20:40:32 +0200

    fix table path

--- components/table-material.js
+++ components/table-material.js
@@ -10,13 +10,13 @@ AFRAME.registerComponent('tablematerial', {
         // map: new THREE.TextureLoader().load("/resources/cube/color.jpg"),
         // metalness: 1.0,
         // color: '0x000000',
-        map: new THREE.TextureLoader().load( `/resources/${table}/col.jpg` ),
+        map: new THREE.TextureLoader().load( `resources/${table}/col.jpg` ),
         // normalScale: new THREE.Vector2( 0.1, 0.1 ),
         // transparent: true,
         // roughness: 1,
         // normalMap: new THREE.TextureLoader().load("nrm.jpg"),
-        roughnessMap: new THREE.TextureLoader().load(`/resources/${table}/refl.jpg`),
-        normalMap: new THREE.TextureLoader().load(`/resources/${table}/nrm.jpg`)
+        roughnessMap: new THREE.TextureLoader().load(`resources/${table}/refl.jpg`),
+        normalMap: new THREE.TextureLoader().load(`resources/${table}/nrm.jpg`)
       });
     },
   
